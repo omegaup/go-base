7746dd186b335e52e3538b42c7689e2bd2f590f1
lhchavez <lhchavez@lhchavez.com>
2019-02-20 19:30:55 -0800

    Move Byte from omegaup/quark/common to omegaup/go-base

---
+++ byte.go
@@ -0,0 +1,123 @@
+package base
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
+)
+
+// A Byte is a unit of digital information.
+type Byte int64
+
+const (
+	// Kibibyte is 1024 Bytes.
+	Kibibyte = Byte(1024)
+
+	// Mebibyte is 1024 Kibibytes.
+	Mebibyte = Byte(1024) * Kibibyte
+
+	// Gibibyte is 1024 Mebibytes.
+	Gibibyte = Byte(1024) * Mebibyte
+
+	// Tebibyte is 1024 Gibibytes.
+	Tebibyte = Byte(1024) * Gibibyte
+)
+
+// MinBytes returns the smaller of x or y.
+func MinBytes(x, y Byte) Byte {
+	if x < y {
+		return x
+	}
+	return y
+}
+
+// MaxBytes returns the larger of x or y.
+func MaxBytes(x, y Byte) Byte {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+// Bytes returns the Byte as an integer number of bytes.
+func (b Byte) Bytes() int64 {
+	return int64(b)
+}
+
+// Kibibytes returns the Byte as an floating point number of Kibibytes.
+func (b Byte) Kibibytes() float64 {
+	return float64(b) / float64(Kibibyte)
+}
+
+// Mebibytes returns the Byte as an floating point number of Mebibytes.
+func (b Byte) Mebibytes() float64 {
+	return float64(b) / float64(Mebibyte)
+}
+
+// Gibibytes returns the Byte as an floating point number of Gibibytes.
+func (b Byte) Gibibytes() float64 {
+	return float64(b) / float64(Gibibyte)
+}
+
+// Tebibytes returns the Byte as an floating point number of Tebibytes.
+func (b Byte) Tebibytes() float64 {
+	return float64(b) / float64(Tebibyte)
+}
+
+// MarshalJSON implements the json.Marshaler interface. The result is an
+// integer number of bytes.
+func (b Byte) MarshalJSON() ([]byte, error) {
+	return []byte(fmt.Sprintf("%d", b.Bytes())), nil
+}
+
+// UnmarshalJSON implements the json.Unmarshaler interface. The result can be
+// an integer number of bytes, or a quoted string that MarshalJSON() can
+// understand.
+func (b *Byte) UnmarshalJSON(data []byte) error {
+	if string(data) == "null" {
+		return nil
+	}
+	if unicode.IsDigit(rune(data[0])) {
+		val, err := strconv.ParseInt(string(data), 10, 64)
+		if err != nil {
+			return nil
+		}
+		*b = Byte(val)
+		return nil
+	}
+	unquoted := string(data)
+	if len(unquoted) < 3 || unquoted[0] != '"' || unquoted[len(unquoted)-1] != '"' {
+		return errors.New("byte: invalid byte " + unquoted)
+	}
+	unquoted = unquoted[1 : len(unquoted)-1]
+	suffixPos := strings.IndexFunc(unquoted, func(c rune) bool {
+		return c != '.' && !unicode.IsDigit(c)
+	})
+	if suffixPos == -1 {
+		suffixPos = len(unquoted)
+	}
+	parsed, err := strconv.ParseFloat(unquoted[:suffixPos], 64)
+	if err != nil {
+		return err
+	}
+	unit := Byte(1)
+	switch string(unquoted[suffixPos:]) {
+	case "TiB":
+		unit = Tebibyte
+	case "GiB":
+		unit = Gibibyte
+	case "MiB":
+		unit = Mebibyte
+	case "KiB":
+		unit = Kibibyte
+	case "B":
+	case "":
+		unit = Byte(1)
+	default:
+		return errors.New("byte: invalid byte " + string(data))
+	}
+	*b = Byte(parsed * float64(unit))
+	return nil
+}
---
+++ byte_test.go
@@ -0,0 +1,110 @@
+package base
+
+import (
+	"testing"
+)
+
+func TestByte(t *testing.T) {
+	testTable := []struct {
+		str      string
+		expected Byte
+	}{
+		{"1", Byte(1)},
+		{"\"10\"", Byte(10)},
+		{"\"100B\"", Byte(100)},
+		{"\"0.5KiB\"", Byte(512)},
+		{"\"1KiB\"", Kibibyte},
+		{"\"1MiB\"", Mebibyte},
+		{"\"1GiB\"", Gibibyte},
+		{"\"1TiB\"", Tebibyte},
+	}
+	for _, entry := range testTable {
+		var b Byte
+		if err := b.UnmarshalJSON([]byte(entry.str)); err != nil {
+			t.Fatalf(err.Error())
+		}
+		if entry.expected != b {
+			t.Errorf("expected %v got %v", entry.expected, b)
+		}
+		marshaled, err := b.MarshalJSON()
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		var b2 Byte
+		if err := b2.UnmarshalJSON(marshaled); err != nil {
+			t.Fatalf(err.Error())
+		}
+		if entry.expected != b2 {
+			t.Errorf("expected %v got %v", entry.expected, b2)
+		}
+	}
+}
+
+func TestByteNil(t *testing.T) {
+	var b, zeroValue Byte
+	if err := b.UnmarshalJSON([]byte("null")); err != nil {
+		t.Fatalf(err.Error())
+	}
+	if zeroValue != b {
+		t.Errorf("expected %v got %v", zeroValue, b)
+	}
+}
+
+func TestByteNumber(t *testing.T) {
+	var b Byte
+	if err := b.UnmarshalJSON([]byte("1099511627776")); err != nil {
+		t.Fatalf(err.Error())
+	}
+	if 1099511627776 != b.Bytes() {
+		t.Errorf("expected 1099511627776 got %v", b)
+	}
+	if 1073741824 != b.Kibibytes() {
+		t.Errorf("expected 1073741824 got %v", b)
+	}
+	if 1048576 != b.Mebibytes() {
+		t.Errorf("expected 1048576 got %v", b)
+	}
+	if 1024 != b.Gibibytes() {
+		t.Errorf("expected 1024 got %v", b)
+	}
+	if 1 != b.Tebibytes() {
+		t.Errorf("expected 1 got %v", b)
+	}
+}
+
+func TestByteInvalidNumber(t *testing.T) {
+	var b, zeroValue Byte
+	if err := b.UnmarshalJSON([]byte("1.x")); err != nil {
+		t.Fatalf(err.Error())
+	}
+	if zeroValue != b {
+		t.Errorf("expected %v got %v", zeroValue, b)
+	}
+}
+
+func TestByteInvalidString(t *testing.T) {
+	var b Byte
+	if err := b.UnmarshalJSON([]byte("\"1")); err == nil {
+		t.Errorf("Expected an error, but got nil")
+	}
+	if err := b.UnmarshalJSON([]byte("\"1.x\"")); err == nil {
+		t.Errorf("Expected an error, but got nil")
+	}
+}
+
+func TestByteComparison(t *testing.T) {
+	b1 := Byte(1)
+	b2 := Byte(2)
+	if b1 != MinBytes(b1, b2) {
+		t.Errorf("expected %v got %v", b1, MinBytes(b1, b2))
+	}
+	if b1 != MinBytes(b2, b1) {
+		t.Errorf("expected %v got %v", b1, MinBytes(b2, b1))
+	}
+	if b2 != MaxBytes(b1, b2) {
+		t.Errorf("expected %v got %v", b2, MaxBytes(b1, b2))
+	}
+	if b2 != MaxBytes(b2, b1) {
+		t.Errorf("expected %v got %v", b2, MaxBytes(b2, b1))
+	}
+}
