d068a21d9289072634578015e3cc368fa3b40263
lhchavez <lhchavez@lhchavez.com>
2021-12-04 06:48:25 -0800

    Add a new logging interface
    The next commit will move the logging implementation to a new
    module. #minor

---
+++ logging/logging.go
@@ -0,0 +1,10 @@
+package logging
+
+// Logger is an interface compatible with newrelic.Logger
+type Logger interface {
+	Error(msg string, context map[string]interface{})
+	Warn(msg string, context map[string]interface{})
+	Info(msg string, context map[string]interface{})
+	Debug(msg string, context map[string]interface{})
+	DebugEnabled() bool
+}
